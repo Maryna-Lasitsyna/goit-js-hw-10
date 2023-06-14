245370ae0d08200cca7644bf82a94d3a0758fa24
Maryna-Lasitsyna <121902242+Maryna-Lasitsyna@users.noreply.github.com>
2023-06-14 16:21:51 +0300

    Исправлены ошибки

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import SlimSelect from 'slim-select';
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-
 const selectEl = document.querySelector('.breed-select');
 const divEl = document.querySelector('.cat-info');
 const loaderEl = document.querySelector('.loader');
@@ -12,7 +11,6 @@ loaderEl.style.display = 'none';
 errorEl.style.display = 'none';
 let breeds = [];
 
-
 function takeBreeds(response) {
   for (let breed in response) {
     breeds.push({ name: response[breed].name, id: response[breed].id });
@@ -20,39 +18,68 @@ function takeBreeds(response) {
 }
 
 async function addBreeds() {
-  const response = await fetchBreeds(errorEl);
-  takeBreeds(response);
-  let listOfBreedsEl = breeds.map(element => {
-    let optionEl = document.createElement('option');
-    optionEl.value = element.id;
-    optionEl.textContent = element.name;
-    return optionEl;
-  });
-  selectEl.append(...listOfBreedsEl);
+  try {
+    const response = await fetchBreeds(errorEl);
+    takeBreeds(response);
+    let listOfBreedsEl = breeds.map(element => {
+      let optionEl = document.createElement('option');
+      optionEl.value = element.id;
+      optionEl.textContent = element.name;
+      return optionEl;
+    });
+    selectEl.append(...listOfBreedsEl);
+  } catch (error) {
+    errorEl.textContent = 'Oops! Something went wrong! Try reloading the page!';
+  }
 }
 
 addBreeds();
 
 function getElements(elements) {
-  const name = elements[0].breeds[0].name;
-  const description = elements[0].breeds[0].description;
-  const temperament = elements[0].breeds[0].temperament;
-  const image = elements[0].url;
-  return {
-    name: name,
-    description: description,
-    temperament: temperament,
-    image: image,
-  };
+  if (elements[0] && elements[0].breeds) {
+    const name = elements[0].breeds[0].name;
+    const description = elements[0].breeds[0].description;
+    const temperament = elements[0].breeds[0].temperament;
+    const image = elements[0].url;
+    return {
+      name: name,
+      description: description,
+      temperament: temperament,
+      image: image,
+    };
+  } else {
+    return null;
+  }
 }
 
 function showBreed(returnedPromise) {
-  const elements = getElements(returnedPromise);
-  const { name, description, temperament, image } = elements;
-  let htmlEls = `<img src="${image}" alt="${name}" class="image"><h1 class="title">${name}</h1><p class="description">${description}</p><p class="temperament"><b class="title-temperament">Temperament: </b>${temperament}</p>`;
-  divEl.innerHTML = htmlEls;
-  loaderSt.style.display = 'none';
-  loaderEl.style.display = 'none';
+  if (!returnedPromise || returnedPromise.length === 0) {
+    divEl.innerHTML = '';
+    divEl.style.display = 'none';
+    loaderSt.style.display = 'none';
+    loaderEl.style.display = 'none';
+    errorEl.textContent = 'Oops! Something went wrong! Try reloading the page!';
+    errorEl.style.display = 'block';
+  } else {
+    const elements = getElements(returnedPromise);
+    if (elements) {
+      const { name, description, temperament, image } = elements;
+      let htmlEls = `<img src="${image}" alt="${name}" class="image"><h1 class="title">${name}</h1><p class="description">${description}</p><p class="temperament"><b class="title-temperament">Temperament: </b>${temperament}</p>`;
+      divEl.innerHTML = htmlEls;
+      loaderSt.style.display = 'none';
+      loaderEl.style.display = 'none';
+      errorEl.style.display = 'none';
+      divEl.style.display = 'block';
+    } else {
+      divEl.innerHTML = '';
+      divEl.style.display = 'none';
+      loaderSt.style.display = 'none';
+      loaderEl.style.display = 'none';
+      errorEl.textContent =
+        'Oops! Something went wrong! Try reloading the page!';
+      errorEl.style.display = 'block';
+    }
+  }
 }
 
 async function onSelectChange(event) {
